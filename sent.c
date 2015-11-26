91aeb01dcef28aea6e15ba7d1c9a094d01dfc41e
Alexis <surryhill@gmail.com>
2015-11-26 15:20:09 +0100

    Refactor die() to use eprintf()

--- sent.c
+++ sent.c
@@ -376,15 +376,9 @@ void die(const char *fmt, ...)
 	va_list ap;
 
 	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
+	eprintf(fmt, ap);
 	va_end(ap);
 
-	if (fmt[0] != '\0' && fmt[strlen(fmt)-1] == ':') {
-		fputc(' ', stderr);
-		perror(NULL);
-	} else {
-		fputc('\n', stderr);
-	}
 	exit(1);
 }
 

268d1968ea951b3eb2d1411a9e0d7c0fcb23217b
Markus Teich <markus.teich@stusta.mhn.de>
2016-03-15 23:51:30 +0100

    fix include and lib paths for the BSDs

--- config.mk
+++ config.mk
@@ -11,8 +11,13 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I. -I/usr/{,local/}include -I/usr/{,local/}include/freetype2 -I${X11INC}
-LIBS = -L/usr/{,local/}lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11
+INCS = -I. -I/usr/include -I/usr/include/freetype2 -I${X11INC}
+LIBS = -L/usr/lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11
+# OpenBSD (uncomment)
+#INCS = -I. -I${X11INC} -I${X11INC}/freetype2
+# FreeBSD (uncomment)
+#INCS = -I. -I/usr/local/include -I/usr/local/include/freetype2 -I${X11INC}
+#LIBS = -L/usr/local/lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_XOPEN_SOURCE=600
